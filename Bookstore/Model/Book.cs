5e7cf73a6688ec9b89db7bcc2d216a69fa27db27
Kağan <kagansensazli@gmail.com>
2024-07-31 20:40:20 +0300

    add existing Bookstore project

---
+++ Bookstore/Model/Book.cs
@@ -0,0 +1,11 @@
+namespace Bookstore.Model;
+
+public class Book
+{
+    public string? Title { get; set; }
+    public int Id { get; set; }
+    public string? Author { get; set; }
+    public int GenreId { get; set; }
+    public int PageCount { get; set; }
+    public DateTime PublishDate { get; set; }
+}
